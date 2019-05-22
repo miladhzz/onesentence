965aaa514e9cb8943eed9ec5efc348c0388c005a
Milad <miladhzz@gmail.com>
2019-05-22 04:56:55 +0430

    update model

--- business/models.py
+++ business/models.py
@@ -34,3 +34,32 @@ class Sentence(models.Model):
     file_type = models.ForeignKey(FileType, on_delete=models.DO_NOTHING)
     file = models.ForeignKey(FileGallery, on_delete=models.DO_NOTHING)
 
+
+class SuggestStatus(models.Model):
+    title = models.CharField(max_length=30)
+
+
+class Suggest(models.Model):
+    create_time = models.DateTimeField(auto_now_add=True)
+    mojri = models.ForeignKey(User, on_delete=models.CASCADE)
+    zaman_pishnahadi = models.IntegerField()
+    mablagh_pishnahadi = models.DecimalField()
+    sentence = models.ForeignKey(Sentence, on_delete=models.CASCADE)
+    description = models.CharField(max_length=300, blank=True)
+    status = models.ForeignKey(SuggestStatus, on_delete=models.DO_NOTHING)
+    score = models.IntegerField()
+    time_tahvil = models.DateTimeField(null=True, blank=True)
+    file = models.ForeignKey(FileGallery, on_delete=models.DO_NOTHING)
+    file_type = models.ForeignKey(FileType, on_delete=models.DO_NOTHING)
+    judgment_request = models.BooleanField()
+    judgment_user_request = models.ForeignKey(User, null=True, blank=True)
+    judgment_description = models.TextField(blank=True, max_length=500)
+
+
+class Judgment(models.Model):
+    suggest = models.ForeignKey(Suggest)
+    description = models.TextField(max_length=500)
+    barandeh = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+
