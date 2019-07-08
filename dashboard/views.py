62bc8130c255c0307d5146fb9ff26abe478a3b08
Milad <miladhzz@gmail.com>
2019-07-08 07:10:33 +0430

    add translator and applicator dashboard view and template

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from business.models import Suggest, Sentence
 # Create your views here.
 
 
 def dashboard_ap(request):
-    return render(request, 'applicant_dashboard.html')
+    my_sentence = Sentence.objects.filter(user=request.user)
+    return render(request, 'applicant_dashboard.html', {'my_sentence': my_sentence})
 
 
+@login_required
 def dashboard_tr(request):
-    return render(request, 'translator_dashboard.html')
+    my_suggests = Suggest.objects.filter(mojri=request.user)
+    return render(request, 'translator_dashboard.html', {'my_suggests': my_suggests})
