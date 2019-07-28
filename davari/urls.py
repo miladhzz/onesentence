ed1243d73fae5d1e6574acd746fd54af6d50edfc
Milad <miladhzz@gmail.com>
2019-07-28 07:19:01 +0430

    create list of judgments

--- davari/urls.py
+++ davari/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 
 from davari import views
 
+
 app_name = 'davari'
 
 urlpatterns = [
     path('dashboard/da/', views.dashboard_da, name='dashboard_da'),
+    path('dashboard/da/<int:suggest_id>', views.sentence_detail_davari,
+         name="sentence_detail_davari"),
+
     ]
--- davari/views.py
+++ davari/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from business.models import Judgment
 
@@ -7,3 +7,9 @@ from business.models import Judgment
 def dashboard_da(request):
     judgments = Judgment.objects.all()
     return render(request, "davar_dashboard.html", {'judgments': judgments})
+
+
+@login_required
+def sentence_detail_davari(request, suggest_id):
+    judgment = get_object_or_404(Judgment, suggest_id=suggest_id)
+    return render(request, "sentence_detail_davari.html", {'judgment': judgment})
