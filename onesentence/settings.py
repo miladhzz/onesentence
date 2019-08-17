8a0fd76e1e71ec4e47174f229f037f0058dc415e
Milad <miladhzz@gmail.com>
2019-08-17 07:05:38 +0430

    change localhost to local ip for connect to mysqlclient

--- onesentence/settings.py
+++ onesentence/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'NAME': 'onesentence',
         'USER': 'root',
         'PASSWORD': '',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
     }
 }
