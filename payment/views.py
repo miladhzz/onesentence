a7653fd997bcf6c204c18b7dd0d3772810b96717
miladhzz <miladhzz@gmail.com>
2019-09-02 18:09:05 +0430

    Add payment forms

--- payment/views.py
+++ payment/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render,get_object_or_404
 from django.contrib.auth.decorators import login_required
-from business.models import Sentence
+from business.models import Sentence, Suggest
+from .forms import PaymentForm
+from django.shortcuts import redirect
 
 
 @login_required
 def init_pay(request):
+    sentence = {}
+    suggest = {}
     if request.method == 'POST':
-        sentence_id = request.POST.get('sentence_id')
-        suggest_price = request.POST.get('suggest_price')
-        sentence = get_object_or_404(Sentence, id=sentence_id)
-        return render(request, "init_pay.html", {"sentence": sentence,
-                                                 "suggest_price": suggest_price})
+        form = PaymentForm(request.POST)
+        if form.is_valid():
+            return redirect('business:home')
     else:
-        return render(request, "init_pay.html")
+        form = PaymentForm()
+        sentence_id = request.GET.get("sentence")
+        suggest_id = request.GET.get("suggest")
+        sentence = get_object_or_404(Sentence, id=sentence_id)
+        suggest = get_object_or_404(Suggest, id=suggest_id, sentence_id=sentence_id)
+
+    return render(request, "init_pay.html", {"sentence": sentence,
+                                             "suggest": suggest,
+                                             "form": form})
