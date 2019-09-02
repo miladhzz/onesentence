d55b8b2048ffcff07657e399df808e3a5c34b328
Milad <miladhzz@gmail.com>
2019-09-02 07:04:28 +0430

    change send method to payment page

--- payment/views.py
+++ payment/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.contrib.auth.decorators import login_required
+from business.models import Sentence
 
 
 @login_required
@@ -7,7 +8,8 @@ def init_pay(request):
     if request.method == 'POST':
         sentence_id = request.POST.get('sentence_id')
         suggest_price = request.POST.get('suggest_price')
-        sentence = 'test sentence'
-        return render(request, "init_pay.html", {"sentence": sentence})
+        sentence = get_object_or_404(Sentence, id=sentence_id)
+        return render(request, "init_pay.html", {"sentence": sentence,
+                                                 "suggest_price": suggest_price})
     else:
         return render(request, "init_pay.html")
