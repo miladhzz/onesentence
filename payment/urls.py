5e1199e75c3d8d49e8ac040cf70b4b93de1b06a4
miladhzz <miladhzz@gmail.com>
2019-09-29 09:23:46 +0330

    Add zarinpal charge

--- payment/urls.py
+++ payment/urls.py
@@ -7,5 +7,6 @@ app_name = 'payment'
 
 urlpatterns = [
     path('payment/', views.init_pay, name='init_pay'),
-
+    path('verify/', views.verify, name='verify'),
+    path('charge/', views.charge, name='charge')
     ]
--- payment/views.py
+++ payment/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from business.models import Sentence, Suggest, Dashboard
 from onesentence.enums import PaymentEnum
 from .forms import PaymentForm
 from django.shortcuts import redirect
 from decimal import Decimal
+from zeep import Client
+from django.http import HttpResponse
 
 
 @login_required
@@ -21,13 +23,47 @@ def init_pay(request):
             mojodi_applicant = Dashboard.objects.get(user=sentence.user).mojodi
             mojodi_translator = Dashboard.objects.get(user=sentence.translator).mojodi
             # todo log transactions
-            Dashboard.objects.filter(user=sentence.user).update(mojodi=mojodi_applicant-Decimal(mablagh))
+            Dashboard.objects.filter(user=sentence.user).update(mojodi=mojodi_applicant - Decimal(mablagh))
             Dashboard.objects.filter(user=sentence.translator).update(mojodi=mojodi_translator + Decimal(mablagh))
             Sentence.objects.filter(id=sentence_id).update(payment_status=PaymentEnum.payed.value)
-            return redirect('dashboard:sentence_detail_dashboard', sentence_id=sentence.id, sentence_title=sentence.title)
+            return redirect('dashboard:sentence_detail_dashboard', sentence_id=sentence.id,
+                            sentence_title=sentence.title)
     else:
         form = PaymentForm()
 
     return render(request, "init_pay.html", {"sentence": sentence,
                                              "suggest": suggest,
                                              "form": form})
+
+
+MERCHANT = ''
+client = Client('https://www.zarinpal.com/pg/services/WebGate/wsdl')
+amount = 1000  # Toman / Required
+description = "تست جنگو"  # Required
+email = 'miladhzz@gmail.com'  # Optional
+mobile = '09384677005'  # Optional
+CallbackURL = 'http://127.0.0.1:8000/verify/'  # Important: need to edit for really server.
+
+
+@login_required
+def charge(request):
+    result = client.service.PaymentRequest(MERCHANT, amount, description, email, mobile, CallbackURL)
+    if result.Status == 100:
+        return redirect('https://www.zarinpal.com/pg/StartPay/' + str(result.Authority))
+    else:
+        return HttpResponse('Error code: ' + str(result.Status))
+
+
+@login_required
+def verify(request):
+    if request.GET.get('Status') == 'OK':
+        result = client.service.PaymentVerification(MERCHANT, request.GET['Authority'], amount)
+        if result.Status == 100:
+            return HttpResponse('Transaction success.\nRefID: ' + str(result.RefID))
+        elif result.Status == 101:
+            return HttpResponse('Transaction submitted : ' + str(result.Status))
+        else:
+            return HttpResponse('Transaction failed.\nStatus: ' + str(result.Status))
+    else:
+        return HttpResponse('Transaction failed or canceled by user')
+
